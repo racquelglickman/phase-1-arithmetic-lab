fa0929cf6a2bf564aa54dbc8d590c77922fdce39
Racquel Glickman <racquel.glickman@gmail.com>
2023-03-20 14:56:19 -0400

    complete arithmetic lab

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 
-// Write your code here
+const num1 = 31;
+const num2 = 2;
+
+let multiply = num1 * num2;
+
+let random = Math.ceil(Math.random() * 10);
+
+const num3 = 52;
+const num4 = 8;
+
+let mod = num3 % num4;
+
+let max = Math.max(3, 7, 12, 20, 15, num2);
